844f994fe71e114b25271bcbec5275fc0fcabd5c
Avi <avimahalingpure10@gmail.com>
2024-07-07 15:37:23 +0530

    delete note feature add

--- index.js
+++ index.js
@@ -248,6 +248,40 @@ app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
     }
 })
 
+// update is pinned
+app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { isPinned } = req.body;
+    const { user } = req.user;
+
+    // if (!isPinned) {
+    //     return res.status(400).json({ error: true, message: "No changes provided" });
+    // }
+
+    try {
+        const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+        if (!note) {
+            return res.status(404).json({ error: true, message: "Note not found" })
+        }
+
+        note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message: "note pinned sucessfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error"
+        })
+    }
+})
+
 app.listen(port, () => {
     console.log(`server listening on http://localhost:${port}`)
 })
